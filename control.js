7d46debc2d973fb425a233a0d6f9b5e8c6bee8ef
Dablac <davidblack@hotmail.com.au>
2019-01-20 15:37:58 +0800

    Update control.js

--- control.js
+++ control.js
@@ -1,3 +1,10 @@
+// ==UserScript==
+// @name         videoControl Testing
+// @author       You
+// @include      *
+// @grant        none
+// ==/UserScript==
+
 const aGet = async (selector, target = document, r = target.querySelector(selector)) => await new Promise((p,f)=>requestAnimationFrame(()=>(!r?f:p)(r))).catch(r=>aGet(selector, target, target.querySelector(selector)));
 
 const profile = {
@@ -61,7 +68,74 @@ const profile = {
     }
 };
 
+function Layers(){
+    this.bottom = !document.getElementsByTagName('IFRAME').length;
+    this.top = window.location === window.parent.location
+    this.middle = !this.bottom && !this.top;
+    this.type = ['bottom', 'top', 'middle'][[this.bottom, this.top, this.middle].indexOf(true)];
+    this.pathDown = void 0;
+    this.pathUp = void 0;
+    this.storageKey = location.pathname+location.search;
+    this._setValue = value => localStorage.setItem(this.storageKey, value);
+    this._getValue = () => localStorage.getItem(this.storageKey);
+    window.addEventListener('message', event=> {
+        let obj = JSON.parse(event.data);
+        if (!!obj && obj.layerStream){
+            //console.log('[%o]%O received message %o; stream = %O', this.type, location.href, obj.data, obj)
+            if (this.bottom || this.top){
+                switch(true){
+                    case obj.data === 'reset':
+                        this.pathDown = void 0;
+                        this.pathUp = void 0;
+                        this.gvCallback = void 0;
+                        this.child = void 0;
+                    case obj.data.startsWith('assignBottom:'):
+                        this.targetBottom = obj.data.slice(13);
+                        this.pathDown = obj.url.concat(location.href).reverse()
+                        console.info('targetBottom & pathDown assigned: %o, this.path = %o', this.targetBottom, this.pathDown);
+                        this.initStream(`pathToTop:${JSON.stringify(obj.url.concat(location.href))}`);
+                        break;
+                    case (obj.data.startsWith('pathToTop:')):
+                        this.pathUp = JSON.parse(obj.data.slice(10));
+                        console.info('pathUp assigned: %o', this.pathUp);
+                        break;
+                    case (obj.data.startsWith('setValue:')):
+                        this._setValue(obj.data.slice(9));
+                        break;
+                    case (this.top && obj.data === 'getValue'):
+                        this.initStream(`getValue:${!this._getValue() ? 0 : this._getValue()}`);
+                        break;
+                    case (obj.data.startsWith('getValue:')):
+                        this.gvCallback(obj.data.slice(9));
+                        break;
+                    default:
+                        throw new Error('no default allowed, error in layer switch statement');
+                        break;
+                }
+            } else this.appendStream(event.data);
+        }
+    });
+    this.sendToParent = (stream, path) => window.parent.postMessage(stream, !path ? document.location.ancestorOrigins.item(document.location.ancestorOrigins.length-1) : path[Math.sign(path.indexOf(location.href))+1]);
+    this.sendToChild = (stream, path, url = new URL(path[Math.sign(path.indexOf(location.href))+1]), child = !this.child ? (this.child = document.querySelector(`iframe[src$="${url.pathname+url.search}"]`)) : this.child) => this.child.contentWindow.postMessage(stream, this.child.src);
+    this.sendStream = (direction, streamString, path) => direction.constructor === Error ? console.error(direction) : (direction === 'Up' ? this.sendToParent : this.sendToChild)(streamString, path);
+    this.appendStream = (_stream, stream = _stream.constructor === String ? JSON.parse(_stream) : stream) =>{
+        stream.url.push(location.href);
+        stream.last = location.href;
+        this.sendStream(stream.direction, JSON.stringify(stream), stream.path);
+    }
+    this.initStream = (data, direction = this.bottom ? 'Up' : (this.top ? 'Down' : new Error('direction must be specified for streams originating in middle layers')), path = this[`path${direction}`]) => this.sendStream(direction, JSON.stringify({direction: direction, path: path, data: data, url: [location.href], initiator: location.href, layerStream: true}), !path ? void 0 : path);
+    this.gvCallback = void 0;
+    this.set = value => this.initStream(`setValue:${value}`);
+    this.get = callback => {
+        this.gvCallback = callback;
+        this.initStream(`getValue`);
+    }
+    this.setBottom = () => this.initStream(`assignBottom:${location.href}`);
+    this.reset = () => this.initStream('reset');
+}
+
 function videoControl(_controllerOptions){
+    this.layers = new Layers();
     this.time = {
         _unit: 'ms',
         units: {ms: 1000, s:1, m:(1/60)},
@@ -119,6 +193,26 @@ function videoControl(_controllerOptions){
             });
         }
     };
+    let startType = void 0;
+    let pausesOnClick = void 0;
+    this.testClickHandler = event =>{
+        if (!startType){
+            startType = event.type;
+            this.video.click();
+        } else if (pausesOnClick === void 0){
+            if (!event.which){
+                if (startType !== event.type){
+                    pausesOnClick = true;
+                    this.options.key.LMB[1] = void 0;
+                    this.options.action[startType === 'play' ? 'play' : 'pause']();
+                    ['play', 'pause'].forEach(type=>this.video.removeEventListener(type, this.testClickHandler, false));
+                }
+            } else {
+                pausesOnClick = false;
+                ['play', 'pause'].forEach(type=>this.video.removeEventListener(type, this.testClickHandler, false));
+            }
+        }
+    }
     this.createController = (forwardedEvent, video, control) => {
         this.options.action = {seek:null, play:null, pause:null, volume:null, muted:null, getCurrentTime:null, currentTime:null, playbackRate:null};
         this.time.unit = this.options.unit;
@@ -126,42 +220,22 @@ function videoControl(_controllerOptions){
         this.setPseudo(video, control);
         this.options.event.types.forEach(type=>window.addEventListener(type, this.controllerHandler, false));
         //if (this.options.fullScreen){}
-        let startType = void 0;
-        let pausesOnClick = void 0;
-        this.testClickHandler = event =>{
-            if (!startType){
-                startType = event.type;
-                video.click();
-            } else if (pausesOnClick === void 0){
-                if (!event.which){
-                    if (startType !== event.type){
-                        pausesOnClick = true;
-                        this.options.key.LMB[1] = void 0;
-                        this.options.action[startType === 'play' ? 'play' : 'pause']();
-                        ['play', 'pause'].forEach(type=>video.removeEventListener(type, this.testClickHandler, false));
-                    }
-                } else {
-                    pausesOnClick = false;
-                    ['play', 'pause'].forEach(type=>video.removeEventListener(type, this.testClickHandler, false));
-                }
-            }
-        }
         ['play', 'pause'].forEach(type=>video.addEventListener(type, this.testClickHandler, false));
         if (this.options.blockContextMenu) window.addEventListener('contextmenu', event=>event.path.includes(this.video) && event.preventDefault(), false);
         if (!!forwardedEvent && !!forwardedEvent.target) forwardedEvent.target.dispatchEvent(forwardedEvent);
     };
-    this.timeSaver = () => this.video.addEventListener('timeupdate', event=>{
-        let gv = +GM_getValue(location.href);
-        if (!gv) GM_setValue(location.href, event.target.currentTime);
+    this.timeSaveHandler = event => this.layers.get(time => {
+        if (!+time) this.layers.set(event.target.currentTime);
         if (!event.target.lastTime){
             event.target.lastTime = 0;
-        } else if (event.target.currentTime < gv){
-            if (event.target.currentTime < event.target.lastTime && event.target.lastTime > 0) GM_setValue(location.href, event.target.currentTime); else this.options.action.set(gv);
-        } else if (event.target.currentTime > gv){
-            GM_setValue(location.href, event.target.currentTime);
+        } else if (event.target.currentTime < +time){
+            if (event.target.currentTime < event.target.lastTime && event.target.lastTime > 0) this.layers.set(event.target.currentTime); else this.options.action.set(+time);
+        } else if (event.target.currentTime > +time){
+            this.layers.set(event.target.currentTime);
         }
         event.target.lastTime = event.target.currentTime;
-    }, false);
+    });
+    this.timeSaver = () => this.video.addEventListener('timeupdate', this.timeSaveHandler, false);
     this.run = event => aGet(':not(a) video:not(.hasVideoController):not(.invalidForVideoController)').then((video, _domain = (this.domain = document.domain), _profile = (this.profile = profile[profile.player[this.domain]]))=>{
         if (!video.closest('a[href]')){
             if (typeof this.profile === 'object'){
@@ -171,8 +245,12 @@ function videoControl(_controllerOptions){
             Object.keys(this.options.mod).forEach(modkey => modkey !== 'active' ? (this.options.valid[modkey] = [modkey, ...Object.keys(this.options.mod[modkey])]) : void 0);
             this.video = video;
             this.video.classList.add('hasVideoController');
-            console.info(this.options);
-            if (this.options.saveTime) this.timeSaver();
+            if (this.options.saveTime){
+                this.layers.reset();
+                this.layers.setBottom();
+                this.timeSaveHandler({target: video});
+                this.timeSaver();
+            }
             this.options.getControl(this.video).then(control=>{
                 this.control = control;
                 this.createController(!event ? null : event, this.video, this.control);
