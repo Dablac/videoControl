a3746a2bd1ee1b2f1a1b72d9e0e4ee6ae6f24bee
Dablac <davidblack@hotmail.com.au>
2019-03-04 14:03:58 +0800

    Update control.js

--- control.js
+++ control.js
@@ -1,11 +1,71 @@
-// ==UserScript==
-// @name         videoControl Testing
-// @author       You
-// @include      *
-// @grant        none
-// ==/UserScript==
-
 const aGet = async (selector, target = document, r = target.querySelector(selector)) => await new Promise((p,f)=>requestAnimationFrame(()=>(!r?f:p)(r))).catch(r=>aGet(selector, target, target.querySelector(selector)));
+
+function Layers(_storageKey){
+    this.storageKey = !_storageKey ? location.pathname+location.search : _storageKey;
+    this.bottom = false;
+    this.top = window.location === window.parent.location
+    this.type = () => ['bottom', 'top', 'middle'][[this.bottom, this.top, !this.bottom && !this.top].indexOf(true)];
+    this.pathDown = void 0;
+    this.pathUp = void 0;
+    this._setValue = value => localStorage.setItem(this.storageKey, value);
+    this._getValue = () => localStorage.getItem(this.storageKey);
+    window.addEventListener('message', event=> {
+        let obj = JSON.parse(event.data);
+        if (!!obj && obj.layerStream){
+            //console.log('[%o]%O received message %o; stream = %O', this.type(), location.href, obj.data, obj)
+            if (this.bottom || this.top){
+                switch(true){
+                    case obj.data === 'reset':
+                        this.pathDown = void 0;
+                        this.pathUp = void 0;
+                        this.gvCallback = void 0;
+                        this.child = void 0;
+                        break;
+                    case obj.data.startsWith('assignBottom:'):
+                        this.targetBottom = obj.data.slice(13);
+                        this.pathDown = obj.url.concat(location.href).reverse()
+                        console.info('targetBottom & pathDown assigned: %o, this.path = %o', this.targetBottom, this.pathDown);
+                        this.initStream(`pathToTop:${JSON.stringify(obj.url.concat(location.href))}`);
+                        break;
+                    case (obj.data.startsWith('pathToTop:')):
+                        this.pathUp = JSON.parse(obj.data.slice(10));
+                        console.info('pathUp assigned: %o', this.pathUp);
+                        break;
+                    case (obj.data.startsWith('setValue:')):
+                        this._setValue(obj.data.slice(9));
+                        break;
+                    case (this.top && obj.data === 'getValue'):
+                        this.initStream(`getValue:${!this._getValue() ? 0 : this._getValue()}`);
+                        break;
+                    case (obj.data.startsWith('getValue:')):
+                        this.gvCallback(obj.data.slice(9));
+                        break;
+                    default:
+                        throw new Error('no default case allowed; unknown error in layer switch-case statement');
+                        break;
+                }
+            } else this.appendStream(event.data);
+        }
+    });
+    this.sendToParent = (stream, path) => window.parent.postMessage(stream, !path ? document.location.ancestorOrigins.item(document.location.ancestorOrigins.length-1) : path[Math.sign(path.indexOf(location.href))+1]);
+    this.sendToChild = (stream, path, url = new URL(path[Math.sign(path.indexOf(location.href))+1]), child = !this.child ? (this.child = document.querySelector(`iframe[src$="${url.pathname+url.search}"]`)) : this.child) => this.child.contentWindow.postMessage(stream, this.child.src);
+    this.sendStream = (direction, streamString, path) => direction.constructor === Error ? console.error(direction) : (direction === 'Up' ? this.sendToParent : this.sendToChild)(streamString, path);
+    this.appendStream = (_stream, stream = _stream.constructor === String ? JSON.parse(_stream) : stream) =>{
+        stream.url.push(location.href);
+        stream.last = location.href;
+        this.sendStream(stream.direction, JSON.stringify(stream), stream.path);
+    }
+    this.initStream = (data, direction = this.bottom ? 'Up' : (this.top ? 'Down' : new Error('direction must be specified for streams originating in middle layers')), path = this[`path${direction}`]) => this.sendStream(direction, JSON.stringify({direction: direction, path: path, data: data, url: [location.href], initiator: location.href, layerStream: true}), !path ? void 0 : path);
+    this.gvCallback = void 0;
+    this.set = value => this.initStream(`setValue:${value}`);
+    this.get = callback => {
+        this.gvCallback = callback;
+        this.initStream(`getValue`);
+    }
+    this.setBottom = (b = (this.bottom = true)) => !(this.flat = (this.top ? true : void 0 !== this.initStream(`assignBottom:${location.href}`)));
+    this.reset = () => this.initStream('reset');
+}
+
 
 const profile = {
     player:{
@@ -18,10 +78,12 @@ const profile = {
     },
     default:{
         unit:'s',
+        //Generally actions with bidirectional outcomes (i.e. forward/backward, slower/faster etc.) default to the negative/decreasing outcome (default input of false) while appending true will switch it to positive/increasing.
         mod:{
-            active:{},//{'LMB': {standard: 'playpause', args: [void 0], used: false}}
+            active:{},//Example format when active: {'LMB': {standard: 'playpause', args: [void 0], used: false}}
             'LMB':{
-                'RMB': ['playpause'],
+                //'RMB': ['skip', 'big', true],
+                'RMB': ['rotate'],
                 'MMB': ['speed', true],
                 'WheelUp': ['speed', false, true],
                 'WheelDown': ['speed', false],
@@ -50,10 +112,18 @@ const profile = {
         key:{
             'ArrowRight': ['skip', 'tiny', true],
             'ArrowLeft': ['skip', 'tiny'],
-            //' ': null,
-            //'LMB': null,
+            ' ': null,
+            'LMB': null,
         },
         ignore: '.vjs-control',
+    },
+    xplayer:{
+        key:{
+            ' ': null,
+            'LMB': ['mod', null],
+        },
+        fullScreen: false,
+        ignore: '.xplayer.no-user-action .control-bar, .xplayer.no-user-action .xp-progress-bar, .xplayer.no-user-action .xplayer-background-top, .xplayer.no-user-action .xplayer-background-bottom, .xplayer.no-user-action .settings-menu, .xplayer.no-user-action .xplayer-hover-menu, .xplayer.no-user-action .xplayer-ads-label, .xplayer.no-user-action .xp-subscribe',
     },
     netflix:{
         unit:'ms',
@@ -62,80 +132,15 @@ const profile = {
             'ArrowLeft': null,
             ' ': null
         },
-        saveTime: true,
+        saveTime: false,
         ignore: '.bottom-controls, top-left-controls',
-        getControl: () => new Promise(gotPlayerInstance=>new Promise(gotVideoPlayer=>gotVideoPlayer(netflix.appContext.state.playerApp.getAPI().videoPlayer)).then(player=>gotPlayerInstance(player.getVideoPlayerBySessionId(player.getAllPlayerSessionIds()[0])))),
+        getControl: () => new Promise(gotPlayerInstance=>new Promise(gotVideoPlayer=>gotVideoPlayer(window.netflix.appContext.state.playerApp.getAPI().videoPlayer)).then(player=>gotPlayerInstance(player.getVideoPlayerBySessionId(player.getAllPlayerSessionIds()[0])))),
     }
 };
 
-function Layers(){
-    this.bottom = !document.getElementsByTagName('IFRAME').length;
-    this.top = window.location === window.parent.location
-    this.middle = !this.bottom && !this.top;
-    this.type = ['bottom', 'top', 'middle'][[this.bottom, this.top, this.middle].indexOf(true)];
-    this.pathDown = void 0;
-    this.pathUp = void 0;
-    this.storageKey = location.pathname+location.search;
-    this._setValue = value => localStorage.setItem(this.storageKey, value);
-    this._getValue = () => localStorage.getItem(this.storageKey);
-    window.addEventListener('message', event=> {
-        let obj = JSON.parse(event.data);
-        if (!!obj && obj.layerStream){
-            //console.log('[%o]%O received message %o; stream = %O', this.type, location.href, obj.data, obj)
-            if (this.bottom || this.top){
-                switch(true){
-                    case obj.data === 'reset':
-                        this.pathDown = void 0;
-                        this.pathUp = void 0;
-                        this.gvCallback = void 0;
-                        this.child = void 0;
-                    case obj.data.startsWith('assignBottom:'):
-                        this.targetBottom = obj.data.slice(13);
-                        this.pathDown = obj.url.concat(location.href).reverse()
-                        console.info('targetBottom & pathDown assigned: %o, this.path = %o', this.targetBottom, this.pathDown);
-                        this.initStream(`pathToTop:${JSON.stringify(obj.url.concat(location.href))}`);
-                        break;
-                    case (obj.data.startsWith('pathToTop:')):
-                        this.pathUp = JSON.parse(obj.data.slice(10));
-                        console.info('pathUp assigned: %o', this.pathUp);
-                        break;
-                    case (obj.data.startsWith('setValue:')):
-                        this._setValue(obj.data.slice(9));
-                        break;
-                    case (this.top && obj.data === 'getValue'):
-                        this.initStream(`getValue:${!this._getValue() ? 0 : this._getValue()}`);
-                        break;
-                    case (obj.data.startsWith('getValue:')):
-                        this.gvCallback(obj.data.slice(9));
-                        break;
-                    default:
-                        throw new Error('no default allowed, error in layer switch statement');
-                        break;
-                }
-            } else this.appendStream(event.data);
-        }
-    });
-    this.sendToParent = (stream, path) => window.parent.postMessage(stream, !path ? document.location.ancestorOrigins.item(document.location.ancestorOrigins.length-1) : path[Math.sign(path.indexOf(location.href))+1]);
-    this.sendToChild = (stream, path, url = new URL(path[Math.sign(path.indexOf(location.href))+1]), child = !this.child ? (this.child = document.querySelector(`iframe[src$="${url.pathname+url.search}"]`)) : this.child) => this.child.contentWindow.postMessage(stream, this.child.src);
-    this.sendStream = (direction, streamString, path) => direction.constructor === Error ? console.error(direction) : (direction === 'Up' ? this.sendToParent : this.sendToChild)(streamString, path);
-    this.appendStream = (_stream, stream = _stream.constructor === String ? JSON.parse(_stream) : stream) =>{
-        stream.url.push(location.href);
-        stream.last = location.href;
-        this.sendStream(stream.direction, JSON.stringify(stream), stream.path);
-    }
-    this.initStream = (data, direction = this.bottom ? 'Up' : (this.top ? 'Down' : new Error('direction must be specified for streams originating in middle layers')), path = this[`path${direction}`]) => this.sendStream(direction, JSON.stringify({direction: direction, path: path, data: data, url: [location.href], initiator: location.href, layerStream: true}), !path ? void 0 : path);
-    this.gvCallback = void 0;
-    this.set = value => this.initStream(`setValue:${value}`);
-    this.get = callback => {
-        this.gvCallback = callback;
-        this.initStream(`getValue`);
-    }
-    this.setBottom = () => this.initStream(`assignBottom:${location.href}`);
-    this.reset = () => this.initStream('reset');
-}
-
 function videoControl(_controllerOptions){
-    this.layers = new Layers();
+    this.storageKey = location.pathname+location.search;
+    this.layers = new Layers(this.storageKey);
     this.time = {
         _unit: 'ms',
         units: {ms: 1000, s:1, m:(1/60)},
@@ -169,7 +174,7 @@ function videoControl(_controllerOptions){
         this.options.action.set = (value) => (!this.options.action.seek.name ? this.options.action.currentTime : this.options.action.seek)(value*this.time.units[this.time._unit]);
         this.options.action.skip = (...args) => (!this.options.action.seek.name ? this.options.action.currentTime : this.options.action.seek)(this.skip(...args));
         this.options.action.playpause = () => video.paused ? this.options.action.play() : this.options.action.pause();
-        this.options.action.rotate = () => (video.style.transform = `rotate(${(+video.style.transform.slice(7, -4)+90)%360}deg)`);
+        this.options.action.rotate = (newDeg = !video.style.transform ? 90 : (parseInt(video.style.transform.substr(7, 3), 10)+90)%360, normal = !(newDeg%180/90)) => (video.style.transform = normal ? `rotate(${newDeg}deg)` : `rotate(${newDeg}deg) scale(${video.videoHeight/video.videoWidth})`);
     };
     this.handlers = {
         mod: (event, pressed, action, args, keyup = event.type.endsWith('up')) =>{
@@ -183,8 +188,11 @@ function videoControl(_controllerOptions){
         },
         down:(event, pressed, action, args, activeMod, state = activeMod ? ([this.options.mod.active[activeMod].used, action, ...args] = [true, ...this.options.mod[activeMod][pressed]]) : void 0) => this.options.action[action](...args),
     }
-    this.controllerHandler = (event, activeMod = Object.keys(this.options.mod.active)[0]) => {
-        if (!document.contains(this.video)) this.run(event); else if ((this.options.fullScreen ? true : event.path.includes(this.video)) && (!['mousedown', 'mouseup', 'wheel'].includes(event.type) || !event.path.some(el=>typeof el.matches === 'function' && el.matches(this.options.ignore)))){
+    this.controllerHandler = (event, activeMod = Object.keys(this.options.mod.active)[0], hitVideo = event.constructor === KeyboardEvent || (this.options.fullScreen ? true : event.path.includes(this.video)), hitIgnore = (hitVideo && event.path.some(el=>typeof el.matches === 'function' && el.matches(this.options.ignore)))) => {
+        if (hitVideo && event.constructor === WheelEvent) event.preventDefault();
+        if (!document.contains(this.video)){
+            this.run(event);
+        } else if (hitVideo && !hitIgnore){
             new Promise(gotKey=>new Promise(gotPressed=>gotPressed(this.options.event.converter[event.type](event))).then(pressed=>gotKey([pressed, ...this.options.key[pressed]]))).then(([pressed, action, ...args])=>{
                 if (this.options.valid[activeMod].includes(pressed)){
                     action === 'mod' ? this.handlers.mod(event, pressed, action, args) : (!event.type.endsWith('up') ? this.handlers.down(event, pressed, action, args, activeMod) : void 0);
@@ -193,49 +201,36 @@ function videoControl(_controllerOptions){
             });
         }
     };
-    let startType = void 0;
-    let pausesOnClick = void 0;
-    this.testClickHandler = event =>{
-        if (!startType){
-            startType = event.type;
-            this.video.click();
-        } else if (pausesOnClick === void 0){
-            if (!event.which){
-                if (startType !== event.type){
-                    pausesOnClick = true;
-                    this.options.key.LMB[1] = void 0;
-                    this.options.action[startType === 'play' ? 'play' : 'pause']();
-                    ['play', 'pause'].forEach(type=>this.video.removeEventListener(type, this.testClickHandler, false));
-                }
-            } else {
-                pausesOnClick = false;
-                ['play', 'pause'].forEach(type=>this.video.removeEventListener(type, this.testClickHandler, false));
-            }
-        }
-    }
     this.createController = (forwardedEvent, video, control) => {
         this.options.action = {seek:null, play:null, pause:null, volume:null, muted:null, getCurrentTime:null, currentTime:null, playbackRate:null};
         this.time.unit = this.options.unit;
         Object.keys(this.options.action).forEach(actionName=>this.options.setAction(video, control, actionName));
         this.setPseudo(video, control);
         this.options.event.types.forEach(type=>window.addEventListener(type, this.controllerHandler, false));
-        //if (this.options.fullScreen){}
-        ['play', 'pause'].forEach(type=>video.addEventListener(type, this.testClickHandler, false));
-        if (this.options.blockContextMenu) window.addEventListener('contextmenu', event=>event.path.includes(this.video) && event.preventDefault(), false);
+        if (this.options.blockContextMenu) video.addEventListener('contextmenu', event => event.preventDefault(), false);
         if (!!forwardedEvent && !!forwardedEvent.target) forwardedEvent.target.dispatchEvent(forwardedEvent);
     };
-    this.timeSaveHandler = event => this.layers.get(time => {
-        if (!+time) this.layers.set(event.target.currentTime);
-        if (!event.target.lastTime){
-            event.target.lastTime = 0;
-        } else if (event.target.currentTime < +time){
-            if (event.target.currentTime < event.target.lastTime && event.target.lastTime > 0) this.layers.set(event.target.currentTime); else this.options.action.set(+time);
-        } else if (event.target.currentTime > +time){
+    this.flatTimeSaveHandler = (event, time = +localStorage[this.storageKey]) => {
+        if (!time) localStorage.setItem(this.storageKey, event.target.currentTime);
+        if (!event.target.lastTime) event.target.lastTime = 0;
+        if (event.target.currentTime < time){
+            if (event.target.currentTime < event.target.lastTime && event.target.lastTime > 0) localStorage.setItem(this.storageKey, event.target.currentTime); else this.options.action.set(time);
+        } else if (event.target.currentTime > time){
+            localStorage.setItem(this.storageKey, event.target.currentTime);
+        }
+        event.target.lastTime = event.target.currentTime;
+    };
+    this.layerTimeSaveHandler = event => this.layers.get((_time, time = +_time) => {
+        if (!time) this.layers.set(event.target.currentTime);
+        if (!event.target.lastTime) event.target.lastTime = 0;
+        if (event.target.currentTime < time){
+            if (event.target.currentTime < event.target.lastTime && event.target.lastTime > 0) this.layers.set(event.target.currentTime); else this.options.action.set(time);
+        } else if (event.target.currentTime > time){
             this.layers.set(event.target.currentTime);
         }
         event.target.lastTime = event.target.currentTime;
     });
-    this.timeSaver = () => this.video.addEventListener('timeupdate', this.timeSaveHandler, false);
+    this.timeSaver = flat => this.video.addEventListener('timeupdate', flat ? this.flatTimeSaveHandler : this.layerTimeSaveHandler, false);
     this.run = event => aGet(':not(a) video:not(.hasVideoController):not(.invalidForVideoController)').then((video, _domain = (this.domain = document.domain), _profile = (this.profile = profile[profile.player[this.domain]]))=>{
         if (!video.closest('a[href]')){
             if (typeof this.profile === 'object'){
@@ -246,10 +241,11 @@ function videoControl(_controllerOptions){
             this.video = video;
             this.video.classList.add('hasVideoController');
             if (this.options.saveTime){
-                this.layers.reset();
-                this.layers.setBottom();
-                this.timeSaveHandler({target: video});
-                this.timeSaver();
+                if (this.layers.setBottom()){
+                    this.layers.reset();
+                    this.timeSaveHandler({target: video}); //init with current video values
+                }
+                this.timeSaver(this.layers.flat);
             }
             this.options.getControl(this.video).then(control=>{
                 this.control = control;
@@ -263,4 +259,4 @@ function videoControl(_controllerOptions){
     this.run();
 };
 
-console.info('new %O', new videoControl());
+console.info('new %O at %o', new videoControl(), location.href);
